85494544ed1b77e2096adee3cac365482bbc50d3
Turbopack <turbo@mail.com>
2025-07-24 22:20:12 -0500

    up

---
+++ Test_Suma.py
@@ -0,0 +1,4 @@
+class Suma_Clase:
+    def suma(a, b):
+        res = a + b
+        return res
---
+++ app.py
@@ -0,0 +1,6 @@
+from Test_Suma import Suma_Clase
+
+def test_suma():
+    CLSuma = Suma_Clase()
+
+print(Suma_Clase.suma(20,5))
